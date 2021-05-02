543e75c5d72002b22c3fc20dab02eab4ca231de8
　citronn <sasaharayuzuki@sy.local>
2021-05-03 02:30:26 +0900

    format settings

--- popup.js
+++ popup.js
@@ -1,25 +1,26 @@
 document.addEventListener(
-  "click",
+  'click',
   function (event) {
-    if (event.target.tagName.toLowerCase != "a") {
-      const dummyTag = document.createElement("p");
-      const uid = "one-clip_" + String(Date.now());
+    if (event.target.tagName.toLowerCase != 'a') {
+      const dummyTag = document.createElement('p');
+      const uid = 'one-clip_' + String(Date.now());
       dummyTag.id = uid;
       dummyTag.innerText = event.target.innerText;
       document.body.appendChild(dummyTag);
 
-      const element = document.querySelector("#" + uid),
+      const element = document.querySelector('#' + uid),
         selection = window.getSelection(),
         range = document.createRange();
       range.selectNodeContents(element);
       selection.removeAllRanges();
       selection.addRange(range);
-      const succeeded = document.execCommand("copy");
-      // if (succeeded) {
-      //   alert("コピーが成功しました！");
-      // } else {
-      //   alert("コピーが失敗しました!");
-      // }
+
+      const succeeded = document.execCommand('copy');
+      if (succeeded) {
+        alert('コピーが成功しました！');
+      } else {
+        alert('コピーが失敗しました!');
+      }
       selection.removeAllRanges();
       document.body.removeChild(dummyTag);
     }
